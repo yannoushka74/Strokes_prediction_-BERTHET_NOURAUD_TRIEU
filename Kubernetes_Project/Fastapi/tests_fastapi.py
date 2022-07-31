b57a5732a8032694be602777a12027bba12ff2a9
GCKA <lnouraud@gmail.com>
2022-07-31 10:00:21 +0000

    Renommage fichier test + implémentation des tests

--- Kubernetes_Project/Fastapi/tests_fastapi.py
+++ Kubernetes_Project/Fastapi/tests_fastapi.py
@@ -1,5 +1,10 @@
 from os.path import exists
 from datetime import date
+import pytest
+import requests
+
+# 1 Tester si fichier CSV est bien présent
+# On vérifie que le CSV est bien présent dans le répertoire (indispensable pour faire tourner le modèle)
 
 
 def is_csv_here(filename):
@@ -34,3 +39,27 @@ def is_csv_here(filename):
                 test_status=test_status,
                 test_date=test_date
             ))
+
+# 2 Tester si données fausses (text au lieu d'int)
+
+
+def test_bad_data_type():
+    url = 'http://localhost:8000/stroke'
+    # On teste de passer du texte pour l'âge là où un float est attendu
+    headers = {
+        "age": "abc",
+        "hypertension": "1",
+        "heart_disease": "1",
+        "ever_married": "1",
+        "Residence_type": "1",
+        "avg_glucose_level": "100",
+        "bmi": "40"
+    }
+    r = requests.get(url, headers=headers)
+    assert r.status_code == 405
+
+# 3 Si fichier dans lequel on sauvegarde le modèle n'existe pas à la base ?
+# def test_exists_saved_ml_file():
+#    url = 'http://localhost:8000/'
+
+# 4 Si on demande les performances avant de générer le modèle ?
